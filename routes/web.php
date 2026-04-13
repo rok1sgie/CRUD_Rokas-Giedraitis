ee23e200bc434233a18df19c2eee96e432ee7c9e
rok1sgie <rokas.giedraitis@stud.svako.lt>
2026-04-13 15:12:31 +0300

    Laravel Breeze diegimas

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,35 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudentController;
+use Illuminate\Support\Facades\Route;
 
-Route::resource('students', StudentController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 
+// Studento sąrašas prieinamas visiems
+Route::get('students', [StudentController::class, 'index'])->name('students.index');
+
+// Visi kiti studentų maršrutai reikalauja autentifikacijos
+Route::middleware('auth')->group(function () {
+      Route::get('students/trashed', [StudentController::class, 'trashed'])->name('students.trashed');
+    Route::post('students/{id}/restore', [StudentController::class, 'restore'])->name('students.restore');
+    Route::delete('students/{id}/forceDelete', [StudentController::class, 'forceDelete'])->name('students.forceDelete');
+  Route::resource('students', StudentController::class)->except(['index']);
+
+});
+
+
+require __DIR__.'/auth.php';
